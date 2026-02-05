cd277190fc25dbb685c1f4785bb32b955730b12f
Nick <melancholic8078@gmail.com>
2026-02-05 23:40:26 +0200

    added async handler for thinner controllers

--- backend/src/controllers/heroController.js
+++ backend/src/controllers/heroController.js
@@ -1,17 +1,13 @@
 import { createHeroService,updateHeroService,deleteHeroService,getHeroDetailsService, getAllHeroesService } from "../services/heroService.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 
-
-export const createHeroController = async (req,res,next)=>{
-   try {
-
+export const createHeroController = asyncHandler(async (req,res)=>{
       const filePath = req.files?.map(file=> `/uploads/${file.filename}`) || null;
-
       const payload = {
          ...req.body,
          images:filePath
       }
-
 
      const answer = await createHeroService(payload)
 
@@ -20,20 +16,13 @@ export const createHeroController = async (req,res,next)=>{
         message:"Hero successfully created",
         data: answer
      })
-    
-   } catch (error) {
-        next(error)
-   }
 }
+)
 
-
-export const updateHeroController = async (req,res,next)=>{
-   try {
-
+export const updateHeroController = asyncHandler(async (req,res)=>{
       const keepImages = req.body.keepImages
       ? JSON.parse(req.body.keepImages)
       : []
-
 
       const newImages = req.files?.map(
          file => `/uploads/${file.filename}`
@@ -52,56 +41,32 @@ export const updateHeroController = async (req,res,next)=>{
          status:200,
          message:"Successfully updated your hero",
          data:answer
-
       })
-
-   } catch (error) {
-      next(error)
-   }
-}
+})
 
 
-export const deleteHeroController = async (req,res,next)=>{
-   try {
-      
+export const deleteHeroController = asyncHandler(async (req,res)=>{
       await deleteHeroService(req.instance)
       return res.status(204).send()
+})
 
-   } catch (error) {
-      next(error)
-   }
-}
-
-export const getHeroDetailsController = async (req,res,next)=>{
-   try {
-      
+export const getHeroDetailsController = asyncHandler( async (req,res)=>{
       const answer = await getHeroDetailsService(req.instance)
       return res.status(200).json({
+         status:200,
          message:"More about this hero",
          data:answer
       })
-     
+})
 
-   } catch (error) {
-      next(error)
-   }
-}
-
-export const getAllHeroesController = async (req,res,next)=>{
-   try {
-
+export const getAllHeroesController = asyncHandler( async (req,res)=>{
       const page = parseInt(req.query.page) || 1;
       const limit = parseInt(req.query.limit) || 5;
 
-      
       const answer = await getAllHeroesService({page,limit})
       return  res.status(200).json({
+         status:200,
          message:"List of all your heroes",
-         response:answer
-
+         data:answer
       })
-
-   } catch (error) {
-      next(error)
-   }
-}
+})
